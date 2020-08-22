bc66aa704c1a9c394e4a409404644f53ce11c7f2
Ilya <cawabunga12@gmail.com>
2020-08-22 15:05:09 +0300

    initial comit

---
+++ Solitaire/Assets/Scripts/Cards/CardsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.Cards
+{
+    public class CardsController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        private RectTransform _rectTransform = null;
+
+        private Transform _originParent = null;
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _originParent = _rectTransform.parent;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            //Отсоединяем от родителя карту
+            _rectTransform.parent = FindObjectOfType<Canvas>().transform;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            //Следование карты за мышью
+            _rectTransform.position = Input.mousePosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+
+        }
+    }
+}
---
+++ Solitaire/Assets/Scripts/Managers/CardDistributor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Managers
+{
+    public class CardDistributor : MonoBehaviour
+    {
+        public GameObject[] prefabCards = new GameObject[52];
+        private GameObject[] _cards = new GameObject[52];
+
+        public Transform canvas;
+
+        private static Transform _parent = null;
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _cards[0] = Instantiate(prefabCards[0], canvas);
+            //_cards[0] = prefabCards[0];
+            //prefabCards[0].transform.position = Vector3.zero;
+
+            for (int i = 1; i < 5; i++)
+            {                
+                _cards[i] = Instantiate(prefabCards[i], _cards[i-1].transform);
+                _cards[i].transform.localPosition = new Vector3(0, -40.0f, 0);
+            }
+        }
+    }
+}
+
+//FindObjectOfType<Canvas>().transform
